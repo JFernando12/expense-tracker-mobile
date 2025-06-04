fa480feb212542ea2893ce7958350c6875c91a10
Fernando Castrejon <fernandocastrejonh@gmail.com>
2025-06-04 14:17:56 -0600

    changes

--- components/statistics/index.ts
+++ components/statistics/index.ts
@@ -1,5 +1,2 @@
 export { default as CategoryDistribution } from './CategoryDistribution';
-export { default as SpendingTrends } from './SpendingTrends';
-export { default as StatisticsChart } from './StatisticsChart';
 export { default as SummaryCards } from './SummaryCards';
-export { default as WalletDistribution } from './WalletDistribution';
